adf1b77c19c95ef179c065e752e330070a33b9dc
Amogha V A <amogha.va.iyer@gmail.com>
2025-04-11 16:33:49 +0530

    Added Signout feature and some minor changes

--- histoury/app/my-journey/page.tsx
+++ histoury/app/my-journey/page.tsx
@@ -1,14 +1,5 @@
-import JourneySidebar from "@/components/JourneySidebar";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import UserProfile from "@/components/UserProfile";
 
 export default function MyJourneyPage() {
-  return (
-    <ProtectedRoute>
-      <div className="min-h-screen bg-amber-50 p-4 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6">
-        <JourneySidebar />
-        <UserProfile />
-      </div>
-    </ProtectedRoute>
-  );
+  return <UserProfile />;
 }
